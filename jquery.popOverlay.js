8cd494852ab19819b7be56fa7b63be2da9d0b0ff
Anson <s1496k030@gmail.com>
2016-07-05 20:42:30 +0800

    Add wait time.

--- jquery.popOverlay.js
+++ jquery.popOverlay.js
@@ -7,43 +7,54 @@
 */
 
 (function($) {
-    $.fn.popup = function (callback, options) {
+    $.popupTotalWait = 0;
+    $.popup = function (callback, options) {
         var _options = {};
         if(!options)  options = {};
-        _options.text = options.text || '';
+        _options.wait = options.wait || 0;
+        _options.text = options.text || 'Hello World!';
         _options.css = options.css || {
             'color': '#fff',
             'font-size': '130%',
             'margin': '4em 10px',
             'text-align': 'center'
         };
+        function generator() {
+            $('.pop-overlay').remove();
+            $('body').append('<div class="pop-overlay">');
+            var popupBox = $('.pop-overlay');
+            popupBox.show();
+            popupBox.height($(window).height());
+            popupBox.width('100%');
+            popupBox.css({
+                'background': 'rgba(0, 0, 0, .7)',
+                'padding': '20px 0',
+                'position': 'fixed',
+                'z-index': '9999',
+                'top': 0,
+                'left': 0
+            });
 
-        $('.pop-overlay').remove();
-        $('body').append('<div class="pop-overlay">');
-        var popupBox = $('.pop-overlay');
-        popupBox.show();
-        popupBox.height($(window).height());
-        popupBox.width('100%');
-        popupBox.css({
-            'background': 'rgba(0, 0, 0, .7)',
-            'padding': '20px 0',
-            'position': 'fixed',
-            'z-index': '9999',
-            'top': 0,
-            'left': 0
-        });
+            popupBox.click(function(e) {
+                if ($(e.target).parent().find(popupBox).length > 0) {
+                    $(popupBox).hide();
+                }
+            });
 
-        popupBox.click(function(e) {
-            if ($(e.target).parent().find(popupBox).length > 0) {
-                $(popupBox).hide();
-            }
-        });
+            var appendBox = $('<div>');
+            appendBox.text(_options.text);
+            appendBox.css(_options.css);
+            popupBox.append(appendBox);
 
-        var appendBox = $('<div>');
-        appendBox.text(_options.text);
-        appendBox.css(_options.css);
-        popupBox.append(appendBox);
+            if('function' === typeof callback) callback(popupBox);
+        }
 
-        if('function' === typeof callback) callback(popupBox);
+        $.popupTotalWait += _options.wait;
+        if(0 === _options.wait) generator();
+        else
+            setTimeout(function() {
+                generator();
+            }, _options.wait);
+        return $;
     };
 }(jQuery));
